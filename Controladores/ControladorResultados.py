5bbeeb66fa668c61123d1031e363c0be873e0861
oyetoto2017 <oyetoto2017@gmail.com>
2022-11-10 12:14:14 -0500

    add: Styles

--- Controladores/ControladorResultados.py
+++ Controladores/ControladorResultados.py
@@ -1,42 +1,38 @@
-from Repositorios.RepositorioMateria import RepositorioMateria
-from Repositorios.RepositorioDepartamento import RepositorioDepartamento
-from Modelos.Departamento import Departamento
-from Modelos.Materia import Materia
-
-class Controladormateria():
+from Modelos.Resultado import Resultado
+class ControladorResultado():
     def __init__(self):
-        print("Creando Controlador Materia")
-        self.repositorioDepartamento = RepositorioDepartamento()
-        self.repositorioMateria = RepositorioMateria()
+        print("Creando Controlador Resultado")
 
-    def crear(self, infoMateria):
-        print("Crear una materia")
-        materia = Materia(infoMateria)
-        return self.repositorioMateria.save(materia)
+    def crear(self, infoResultado):
+        print("Crear un Resultado")
+        resultado = Resultado(infoResultado)
+        return resultado.__dict__
 
-    def mostrarMateria(self, id):
-        print("Mostrando la materia con id: "+id)
-        materia = Materia(self.repositorioMateria.findById(id))
-        return materia.__dict__
+    def mostrarResultado(self, id):
+        print("Mostrando el resultado con id: "+id)
+        resultado={
+            "_id":id,
+            "numero_mesa":"12",
+            "id_candidato":"3",
 
-    def mostrarMaterias(self):
-        print("Mostrando todas las materias")
-        return self.repositorioMateria.findAll()
+        }
+        return resultado
 
-    def actualizar(self,id,infoMateria):
-        print("Actualizando la materia con id: "+id)
-        materiaActual = Materia(self.repositorioMateria.findById(id))
-        materiaActual.nombre = infoMateria["nombre"]
-        materiaActual.creditos = infoMateria["creditos"]
-        return self.repositorioMateria.save(materiaActual)
+    def mostrarResultados(self):
+        print("Mostrando todos los resultados")
+        unResultado = {
+            "_id":"123",
+            "numero_mesa":"12",
+            "id_candidato":"3",
+
+        }
+        return [unResultado]
+
+    def actualizar(self,id,infoResultado):
+        print("Actualizando el resultado con id: "+id)
+        resultado = Resultado(infoResultado)
+        return resultado.__dict__
 
     def eliminar(self,id):
-        print("eliminando la materia con id: "+id)
-        return self.repositorioMateria.delete(id)
-
-    #Relación entre materia y departamento
-    def asignarDepartamento(self,id, id_departamento):
-        materiaActual = Materia(self.repositorioMateria.findById(id))
-        departamentoActual = Departamento(self.repositorioDepartamento.findById(id_departamento))
-        materiaActual.departamento = departamentoActual
-        return self.repositorioMateria.save(materiaActual)
+        print("eliminando el resultado con id: "+id)
+        return {"deleted_count":1}
