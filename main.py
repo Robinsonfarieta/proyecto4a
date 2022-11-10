22228c85be2731d9609836d83637715895c67f07
oyetoto2017 <oyetoto2017@gmail.com>
2022-11-10 12:53:52 -0500

    add: Styles

--- main.py
+++ main.py
@@ -5,61 +5,57 @@ from flask_cors import CORS
 import json
 from waitress import serve
 
-from Controladores.ControladorCandidato import ControladorCandidato
+from Controladores.ControladorResultado import ControladorResultado
 import pymongo
 import certifi
 
 app = Flask(__name__)
 cors = CORS(app)
-miCandidato = ControladorCandidato()
+miControladorResultado = ControladorResultado()
 
-
-@app.route("/", methods=['GET'])
+#LINEA ADICIONAL
+@app.route("/",methods=["GET"])
 def test():
-    json = {}
-    json['mensaje'] = "Servidor ejecutándose"
+    json={}
+    json ["mensaje"]= "Servidor ejecutandose"
     return jsonify(json)
 
 
-@app.route("/candidato", methods=['POST'])
-def crearcandidatos():
+#LINEA RESULTADOS
+@app.route("/resultados",methods=["POST"])
+def crearResultado():
     data = request.get_json()
-    respuestacrear = miCandidato.crearcandidato(data)
-    return jsonify(respuestacrear)
+    respuestaCrear = miControladorResultado.crear(data)
+    return jsonify(respuestaCrear)
 
-
-@app.route("/candidato/<string:id>", methods=['GET'])
-def consultarCandidato(id):
-    respuestaConsultar = miCandidato.mostrarcandidato(id)
+@app.route("/resultados/<string:id>", methods =["GET"])
+def consultarResultado(id):
+    respuestaConsultar = miControladorResultado.mostrarResultado(id)
     return jsonify(respuestaConsultar)
 
-
-@app.route("/candidato", methods=['GET'])
-def consultarCandidatos():
-    respuestaConsultar = miCandidato.mostrarcandidatos()
+@app.route("/resultados",methods=["GET"])
+def consultarResultados():
+    respuestaConsultar = miControladorResultado.mostrarResultados()
     return jsonify(respuestaConsultar)
 
-
-@app.route("/candidato/<string:id>", methods=['PUT'])
-def actualizarCandidatos(id):
+@app.route("/resultados/<string:id>", methods = ["PUT"])
+def actualizarResultado(id):
     datos = request.get_json()
-    respuestaActualizar = miCandidato.actualizar(id, datos)
+    respuestaActualizar = miControladorResultado.actualizar(id,datos)
     return jsonify(respuestaActualizar)
 
-
-@app.route("/candidato/<string:id>", methods=['DELETE'])
-def eliminarCandidatos(id):
-    respuestaEliminar = miCandidato.eliminar(id)
+@app.route("/resultados/<string:id>", methods = ["DELETE"])
+def eliminarResultado(id):
+    respuestaEliminar = miControladorResultado.eliminar(id)
     return jsonify(respuestaEliminar)
 
-
+#LINEA ADICIONAL
 def loadFileConfig():
-    with open('config.json') as f:
+    with open("config.json") as f:
         data = json.load(f)
     return data
 
-
-if __name__ == '__main__':
+if __name__=="__main__":
     dataConfig = loadFileConfig()
-    print("Servidor corriendo en: " + dataConfig['url-backend'] + " puerto: " + str(dataConfig['port']))
-    serve(app, host=dataConfig['url-backend'], port=dataConfig['port'])
+    print("servidor corriendo en: "+dataConfig["url-backend"]+ " puerto: "+str(dataConfig["port"]))
+    serve(app, host = dataConfig["url-backend"], port = dataConfig["port"])
